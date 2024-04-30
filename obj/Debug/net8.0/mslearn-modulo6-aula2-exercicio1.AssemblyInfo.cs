64be673aab372e4d19b3a79258c28b6a84759350
Wagner <wagner_silvajr@hotmail.com>
2024-04-29 21:23:38 -0300

    testando o try catch com nivel acima de na pilha de chamada

--- obj/Debug/net8.0/mslearn-modulo6-aula2-exercicio1.AssemblyInfo.cs
+++ obj/Debug/net8.0/mslearn-modulo6-aula2-exercicio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mslearn-modulo6-aula2-exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436cc3d677d95807bd2e7f1b6a9ef82665b63782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef4cfb7c3aafa2a68a65ea117442baca3cf64e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("mslearn-modulo6-aula2-exercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mslearn-modulo6-aula2-exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
