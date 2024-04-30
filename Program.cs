fef4cfb7c3aafa2a68a65ea117442baca3cf64e5
Wagner <wagner_silvajr@hotmail.com>
2024-04-29 21:21:44 -0300

    testando try catch

--- Program.cs
+++ Program.cs
@@ -3,6 +3,15 @@ double float2 = 0.0;
 int number1 = 3000;
 int number2 = 0;
 
+try
+{
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}
+catch
+{
+    System.Console.WriteLine("An exception has been caught");
+}
 
 Console.WriteLine("Exit program");
 
